7039ebe8191c8414ba9be43b17520ec679660c9b
marciojc <marciojc@gmail.com>
2018-10-17 23:46:17 +0100

    Convert header

--- inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -72,8 +72,8 @@ add_action( 'storefront_after_footer',                     'storefront_sticky_si
  * @see storefront_product_search()
  * @see storefront_header_cart()
  */
-add_action( 'storefront_header', 'storefront_product_search', 40 );
-add_action( 'storefront_header', 'storefront_header_cart',    60 );
+add_action( 'storefront_header_top', 'storefront_product_search', 40 );
+add_action( 'storefront_header_bottom', 'storefront_header_cart',    60 );
 
 /**
  * Cart fragment
